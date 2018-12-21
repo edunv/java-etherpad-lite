7f7f5e7acd71c0493081f30d62a5bc41f3c2f9d7
Eduardo Núñez Vázquez <eduardo.nunez1@udc.es>
2018-12-21 15:53:30 +0100

    Pit y no GraphWalker

--- src/test/java/net/gjerull/etherpad/client/EPLiteClientIntegrationTest.java
+++ src/test/java/net/gjerull/etherpad/client/EPLiteClientIntegrationTest.java
@@ -18,12 +18,18 @@ import org.mockserver.integration.ClientAndServer;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 import org.mockserver.matchers.Times;
 
+import etm.core.monitor.EtmMonitor;
+import etm.core.configuration.EtmManager;
+import etm.core.configuration.BasicEtmConfigurator;
+import etm.core.renderer.SimpleTextRenderer;
+
 /**
  * Integration test for simple App.
  */
 public class EPLiteClientIntegrationTest {
 	private EPLiteClient client;
 	private ClientAndServer mockServer;
+	private EtmMonitor monitor;
 
 	/**
 	 * Useless testing as it depends on a specific API key
@@ -36,12 +42,22 @@ public class EPLiteClientIntegrationTest {
 				"http://localhost:9001",
 				"a04f17343b51afaa036a7428171dd873469cd85911ab43be0503d29d2acbbd58"
 		);
+		
+		((ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory
+		         .getLogger("org.mockserver.mock"))
+		            .setLevel(ch.qos.logback.classic.Level.OFF);
 		mockServer = startClientAndServer(9001);
+		
+		BasicEtmConfigurator.configure();
+        monitor = EtmManager.getEtmMonitor();
+        monitor.start();
 	}
 
 	@After
 	public void tearDown() {
 		mockServer.stop();
+		monitor.render(new SimpleTextRenderer());
+        monitor.stop();
 	}
 
 	@Test
